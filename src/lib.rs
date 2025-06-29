15955c6245a86e44a1c7b6a309c40ed61f510b85
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-06-29 23:08:02 +0200

    impl: check_crc, constants

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,15 @@ mod crc;
 
 use pyo3::prelude::*;
 use protocol::*;
-use crate::crc::crc16;
 
 #[pymodule]
 fn proto170(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(crc16, m)?)?;
+    m.add_class::<PacketConstants>()?;
     m.add_function(wrap_pyfunction!(build_request, m)?)?;
     m.add_function(wrap_pyfunction!(build_ping_request, m)?)?;
     m.add_function(wrap_pyfunction!(build_register_request, m)?)?;
     m.add_function(wrap_pyfunction!(build_read_request, m)?)?;
     m.add_function(wrap_pyfunction!(build_set_request, m)?)?;
+    m.add_function(wrap_pyfunction!(check_crc, m)?)?;
     Ok(())
 }
