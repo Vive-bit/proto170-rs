8b64e7dcf86e50b9e3f7cf4e4b32ef7c170e7559
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-06-29 15:25:57 +0200

    update: lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-pub mod packet;
+mod protocol;
+mod util;
 mod crc;
 
 use pyo3::prelude::*;
+use protocol::*;
 
-/// python module
 #[pymodule]
 fn proto170(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(packet::build_request, m)?)?;
+    m.add_function(wrap_pyfunction!(build_request, m)?)?;
+    m.add_function(wrap_pyfunction!(build_ping_request, m)?)?;
+    m.add_function(wrap_pyfunction!(build_register_request, m)?)?;
+    m.add_function(wrap_pyfunction!(build_read_request, m)?)?;
     Ok(())
 }
