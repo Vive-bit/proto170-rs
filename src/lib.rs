e510ec0b457ccb79cba0b6f0cd41fd7116a1e668
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-07-12 13:52:14 +0200

    refac: lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,8 @@ use protocol::*;
 #[pymodule]
 fn proto170(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PacketConstants>()?;
-    m.add_function(wrap_pyfunction!(build_request, m)?)?;
-    m.add_function(wrap_pyfunction!(build_ping_request, m)?)?;
-    m.add_function(wrap_pyfunction!(build_register_request, m)?)?;
-    m.add_function(wrap_pyfunction!(build_read_request, m)?)?;
-    m.add_function(wrap_pyfunction!(build_set_request, m)?)?;
+    m.add_function(wrap_pyfunction!(build_request_frame, m)?)?;
+    m.add_function(wrap_pyfunction!(build_response_frame, m)?)?;
     m.add_function(wrap_pyfunction!(check_crc, m)?)?;
     Ok(())
 }
