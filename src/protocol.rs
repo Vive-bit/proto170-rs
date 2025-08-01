db1149027a01fec19ed0fa48f0c5521b2a668e7b
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-08-01 20:02:11 +0200

    refac > modular

--- src/protocol.rs
+++ src/protocol.rs
@@ -5,8 +5,6 @@ use crate::util::{pack, unpack};
 pub const START_BYTE: u8 = 0x7E;
 pub const REQUEST_FRAME_TYPE: u8 = 0x10;
 pub const RESPONSE_FRAME_TYPE: u8 = 0x20;
-pub const REQUEST_FRAME_LENGTH: usize = 13;
-pub const RESPONSE_FRAME_LENGTH: usize = 10;
 
 
 #[pyclass]
@@ -20,38 +18,18 @@ impl PacketConstants {
     pub const REQUEST_FRAME_TYPE: u8 = REQUEST_FRAME_TYPE;
     #[classattr]
     pub const RESPONSE_FRAME_TYPE: u8 = RESPONSE_FRAME_TYPE;
-    #[classattr]
-    pub const REQUEST_FRAME_LENGTH: usize = REQUEST_FRAME_LENGTH;
-    #[classattr]
-    pub const RESPONSE_FRAME_LENGTH: usize = RESPONSE_FRAME_LENGTH;
 }
 
 #[pyfunction]
-pub fn build_request_frame(
-    request_id: u16,
-    slave_id: u8,
-    request_type: u8,  // 0x01 = GET
-    operation: u8,     // 0x00 = READ, 0x01 = WRITE
-    mode: u8,          // 0x00 = DIGITAL, 0x01 = ANALOG
-    new_state: u16,    // nur bei WRITE relevant
-    gpio_id: u8,
-) -> Vec<u8> {
-    let mut frame = Vec::with_capacity(REQUEST_FRAME_LENGTH);
+fn build_frame(frame_type: u8, payload: Vec<u8>) -> Vec<u8> {
+    let length = (payload.len() + 2) as u8;
+
+    let mut frame = Vec::with_capacity(3 + payload.len() + 2);
     frame.push(START_BYTE);
-    frame.push(REQUEST_FRAME_TYPE);
+    frame.push(frame_type);
+    frame.push(length);
 
-    frame.push((request_id >> 8) as u8);
-    frame.push((request_id & 0xFF) as u8);
-
-    frame.push(slave_id);
-    frame.push(request_type);
-    frame.push(operation);
-    frame.push(mode);
-
-    frame.push((new_state >> 8) as u8);
-    frame.push((new_state & 0xFF) as u8);
-
-    frame.push(gpio_id);
+    frame.extend_from_slice(&payload); 
 
     let crc = crc16(&frame);
     frame.push((crc & 0xFF) as u8);
@@ -61,27 +39,24 @@ pub fn build_request_frame(
 }
 
 #[pyfunction]
-pub fn build_response_frame(
+pub fn build_request_frame(
     request_id: u16,
     slave_id: u8,
-    data_value: u16,
+    request_type: u8,
+    operation: u8,
+    mode: u8,
+    payload: Vec<u8>
 ) -> Vec<u8> {
-    let mut frame = Vec::with_capacity(RESPONSE_FRAME_LENGTH);
-    frame.push(START_BYTE);
-    frame.push(RESPONSE_FRAME_TYPE);
-
+    let mut frame = Vec::with_capacity(payload.len() + 6);
     frame.push((request_id >> 8) as u8);
     frame.push((request_id & 0xFF) as u8);
 
     frame.push(slave_id);
-    frame.push(0x02);
+    frame.push(request_type);
+    frame.push(operation);
+    frame.push(mode);
 
-    frame.push((data_value >> 8) as u8);
-    frame.push((data_value & 0xFF) as u8);
-
-    let crc = crc16(&frame);
-    frame.push((crc & 0xFF) as u8);
-    frame.push((crc >> 8) as u8);
+    frame.extend_from_slice(&payload); 
 
     frame
 }
