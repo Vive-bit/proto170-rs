f2d35cf26aa484269816bf43a62a35dfbb0db8e4
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-08-01 20:03:55 +0200

    PyO3-inize

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,9 @@
+#[pyfunction]
 pub fn pack(a: u8, b: u8) -> u16 {
     ((a as u16) << 8) | b as u16
 }
 
+#[pyfunction]
 pub fn unpack(val: u16) -> (u8, u8) {
     ((val >> 8) as u8, (val & 0xFF) as u8)
 }
