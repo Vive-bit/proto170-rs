8ecac08ac9cfc4f376fe6337de5842188c5e01b2
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-08-01 20:06:02 +0200

    adjust

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,15 @@ mod crc;
 
 use pyo3::prelude::*;
 use protocol::*;
+use util::*;
 
 #[pymodule]
 fn proto170(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PacketConstants>()?;
     m.add_function(wrap_pyfunction!(build_request_frame, m)?)?;
-    m.add_function(wrap_pyfunction!(build_response_frame, m)?)?;
+    m.add_function(wrap_pyfunction!(build_frame, m)?)?;
     m.add_function(wrap_pyfunction!(check_crc, m)?)?;
+    m.add_function(wrap_pyfunction!(pack, m)?)?;
+    m.add_function(wrap_pyfunction!(unpack, m)?)?;
     Ok(())
 }
